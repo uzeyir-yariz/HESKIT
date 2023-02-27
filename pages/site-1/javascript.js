4bf187db08e4dc729572103e2538fc35d0f097b2
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2023-02-27 15:49:38 +0300

    ver 0.2 beta update
    bu güncelleme ile html css için çalışmalar başlatıldı

---
+++ pages/site-1/javascript.js
@@ -0,0 +1,9 @@
+// hesaplama bölümü 
+
+// 5 yıldan fazla kirada ise %50 zam yapabilir 
+// 5 yıldan az kirada ise %25 zam yapabilir
+
+function hesapla(){
+    let yil_list = document.createElement("option");
+    console.log(yil_list);
+}
