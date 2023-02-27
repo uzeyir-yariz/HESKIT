2454b75583a511b29f686a895a9dc864d924c457
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2023-02-27 21:23:54 +0300

    update ver 0.3

--- pages/site-1/javascript.js
+++ pages/site-1/javascript.js
@@ -4,6 +4,21 @@
 // 5 yıldan az kirada ise %25 zam yapabilir
 
 function hesapla(){
-    let yil_list = document.createElement("option");
-    console.log(yil_list);
+    // değişkenler
+    
+    var yilList = document.getElementById("yil_list");
+    let select_yil = yilList.options[yilList.selectedIndex].value;
+    // console.log(selectyil); [çalışıyor :D]
+
+    var ayList = document.getElementById("ay_list");
+    let select_ay = ayList.options[ayList.selectedIndex].value;
+    // console.log(select_ay); [çalışıyor :D]
+
+    const now = new Date();
+    const YIL = now.getFullYear();
+    const AY = now.getMonth() + 1;
+
+    var topYil = select_yil - YIL;
+    var topAy = select_ay - AY;
+
 }
