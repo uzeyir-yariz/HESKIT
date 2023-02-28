3224277ac56a5a9d28869a966696e84d084c49d7
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2023-02-28 11:56:14 +0300

    update ver 0.4 beta
    gereksiz dosyalar kaldırıldı

--- pages/site-1/javascript.js
+++ pages/site-1/javascript.js
@@ -1,24 +1,65 @@
-// hesaplama bölümü 
-
-// 5 yıldan fazla kirada ise %50 zam yapabilir 
-// 5 yıldan az kirada ise %25 zam yapabilir
-
 function hesapla(){
-    // değişkenler
-    
-    var yilList = document.getElementById("yil_list");
-    let select_yil = yilList.options[yilList.selectedIndex].value;
-    // console.log(selectyil); [çalışıyor :D]
-
-    var ayList = document.getElementById("ay_list");
-    let select_ay = ayList.options[ayList.selectedIndex].value;
-    // console.log(select_ay); [çalışıyor :D]
-
-    const now = new Date();
-    const YIL = now.getFullYear();
-    const AY = now.getMonth() + 1;
-
-    var topYil = select_yil - YIL;
-    var topAy = select_ay - AY;
-
+    let yillist = document.getElementById("yil_list");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // hesaplama bölümü 
+// // 5 yıldan fazla kirada ise %50 zam yapabilir 
+// // 5 yıldan az kirada ise %25 zam yapabilir
+
+// function hesapla(){
+//     // değişkenler
+    
+//     var yilList = document.getElementById("yil_list");
+//     let select_yil = yilList.options[yilList.selectedIndex].value;
+//     // console.log(selectyil); [çalışıyor :D]
+
+//     var ayList = document.getElementById("ay_list");
+//     let select_ay = ayList.options[ayList.selectedIndex].value;
+//     // console.log(select_ay); [çalışıyor :D]
+
+//     const now = new Date();
+//     const YIL = now.getFullYear();
+//     const AY = now.getMonth() + 1;
+//     // console.log(YIL); [çalışıyor :D]
+//     // console.log(AY); [çalışıyor :D]
+//     // var topYil = select_yil - YIL;
+//     // var topAy = select_ay - AY;
+//     // var fark = topAy + (topYil * 12);
+
+//     let mevcutkira,yenikira;
+
+//     mevcutkira = documnet.getElementById("kira").value; 
+    
+    
+    
+//     /*
+//     if(fark >= 60){
+//         console.log("hello world");
+//     }
+
+//     else if(fark < 60 && fark >= 12){
+//         console.log("hello world");
+//     }
+
+//     else{
+//         console.log("bu terslikte bir iş var");
+//     }
+//     */
+// }
